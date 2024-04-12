f387daa96da4f6605a11d2c11cce22bc9ee5a914
RTann <rtannenb@redhat.com>
2024-04-12 10:11:06 -0700

    chore: remove import aliases

--- registry/manifest.go
+++ registry/manifest.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 
 	"github.com/docker/distribution/manifest/manifestlist"
-	ociV1 "github.com/docker/distribution/manifest/ocischema"
-	manifestV1 "github.com/docker/distribution/manifest/schema1"
-	manifestV2 "github.com/docker/distribution/manifest/schema2"
+	"github.com/docker/distribution/manifest/ocischema"
+	"github.com/docker/distribution/manifest/schema1"
+	"github.com/docker/distribution/manifest/schema2"
 	"github.com/opencontainers/go-digest"
 )
 
@@ -22,12 +22,12 @@ const (
 	MediaTypeImageIndex = "application/vnd.oci.image.index.v1+json"
 )
 
-func (registry *Registry) Manifest(repository, reference string) (*manifestV1.SignedManifest, error) {
-	return registry.v1Manifest(repository, reference, manifestV1.MediaTypeManifest)
+func (registry *Registry) Manifest(repository, reference string) (*schema1.SignedManifest, error) {
+	return registry.v1Manifest(repository, reference, schema1.MediaTypeManifest)
 }
 
-func (registry *Registry) SignedManifest(repository, reference string) (*manifestV1.SignedManifest, error) {
-	return registry.v1Manifest(repository, reference, manifestV1.MediaTypeSignedManifest)
+func (registry *Registry) SignedManifest(repository, reference string) (*schema1.SignedManifest, error) {
+	return registry.v1Manifest(repository, reference, schema1.MediaTypeSignedManifest)
 }
 
 func (registry *Registry) ManifestList(repository, reference string) (*manifestlist.DeserializedManifestList, error) {
@@ -59,7 +59,7 @@ func (registry *Registry) ManifestList(repository, reference string) (*manifestl
 	return deserialized, nil
 }
 
-func (registry *Registry) v1Manifest(repository, reference string, mediaType string) (*manifestV1.SignedManifest, error) {
+func (registry *Registry) v1Manifest(repository, reference string, mediaType string) (*schema1.SignedManifest, error) {
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
 	registry.Logf("registry.manifest.get url=%s repository=%s reference=%s", url, repository, reference)
 
@@ -80,7 +80,7 @@ func (registry *Registry) v1Manifest(repository, reference string, mediaType str
 		return nil, err
 	}
 
-	signedManifest := &manifestV1.SignedManifest{}
+	signedManifest := &schema1.SignedManifest{}
 	err = signedManifest.UnmarshalJSON(body)
 	if err != nil {
 		return nil, err
@@ -89,7 +89,7 @@ func (registry *Registry) v1Manifest(repository, reference string, mediaType str
 	return signedManifest, nil
 }
 
-func (registry *Registry) ManifestV2(repository, reference string) (*manifestV2.DeserializedManifest, error) {
+func (registry *Registry) ManifestV2(repository, reference string) (*schema2.DeserializedManifest, error) {
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
 	registry.Logf("registry.manifest.get url=%s repository=%s reference=%s", url, repository, reference)
 
@@ -98,7 +98,7 @@ func (registry *Registry) ManifestV2(repository, reference string) (*manifestV2.
 		return nil, err
 	}
 
-	req.Header.Set("Accept", manifestV2.MediaTypeManifest)
+	req.Header.Set("Accept", schema2.MediaTypeManifest)
 	resp, err := registry.Client.Do(req)
 	if err != nil {
 		return nil, err
@@ -110,7 +110,7 @@ func (registry *Registry) ManifestV2(repository, reference string) (*manifestV2.
 		return nil, err
 	}
 
-	deserialized := &manifestV2.DeserializedManifest{}
+	deserialized := &schema2.DeserializedManifest{}
 	err = deserialized.UnmarshalJSON(body)
 	if err != nil {
 		return nil, err
@@ -147,7 +147,7 @@ func (registry *Registry) ImageIndex(repository, reference string) (*manifestlis
 	return deserialized, nil
 }
 
-func (registry *Registry) ManifestOCI(repository, reference string) (*ociV1.DeserializedManifest, error) {
+func (registry *Registry) ManifestOCI(repository, reference string) (*ocischema.DeserializedManifest, error) {
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
 	registry.Logf("registry.manifest.get url=%s repository=%s reference=%s", url, repository, reference)
 
@@ -168,7 +168,7 @@ func (registry *Registry) ManifestOCI(repository, reference string) (*ociV1.Dese
 		return nil, err
 	}
 
-	deserialized := &ociV1.DeserializedManifest{}
+	deserialized := &ocischema.DeserializedManifest{}
 	err = deserialized.UnmarshalJSON(body)
 	if err != nil {
 		return nil, err
@@ -185,9 +185,9 @@ func (registry *Registry) ManifestDigest(repository, reference string) (digest.D
 		return "", "", err
 	}
 
-	req.Header.Add("Accept", manifestV2.MediaTypeManifest)
-	req.Header.Add("Accept", manifestV1.MediaTypeManifest)
-	req.Header.Add("Accept", manifestV1.MediaTypeSignedManifest)
+	req.Header.Add("Accept", schema2.MediaTypeManifest)
+	req.Header.Add("Accept", schema1.MediaTypeManifest)
+	req.Header.Add("Accept", schema1.MediaTypeSignedManifest)
 	req.Header.Add("Accept", manifestlist.MediaTypeManifestList)
 	req.Header.Add("Accept", MediaTypeImageManifest)
 	req.Header.Add("Accept", MediaTypeImageIndex)
@@ -223,7 +223,7 @@ func (registry *Registry) DeleteManifest(repository string, digest digest.Digest
 	return nil
 }
 
-func (registry *Registry) PutManifest(repository, reference string, signedManifest *manifestV1.SignedManifest) error {
+func (registry *Registry) PutManifest(repository, reference string, signedManifest *schema1.SignedManifest) error {
 	url := registry.url("/v2/%s/manifests/%s", repository, reference)
 	registry.Logf("registry.manifest.put url=%s repository=%s reference=%s", url, repository, reference)
 
@@ -238,7 +238,7 @@ func (registry *Registry) PutManifest(repository, reference string, signedManife
 		return err
 	}
 
-	req.Header.Set("Content-Type", manifestV1.MediaTypeManifest)
+	req.Header.Set("Content-Type", schema1.MediaTypeManifest)
 	resp, err := registry.Client.Do(req)
 	if resp != nil {
 		defer resp.Body.Close()
